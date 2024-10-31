68d2a97bae3177b3a8e4b5f5606180265258322a
tilak-dev <tsingh99991@gmail.com>
2024-10-31 10:50:38 +0530

    signup page added working fine

--- client/src/app/(aurh)/signup/page.tsx
+++ client/src/app/(aurh)/signup/page.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -16,40 +15,59 @@ import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import api from "@/utils/axiosInstance";
 
 const SignupPage = () => {
-  const [avatar, setAvatar] = useState();
-
-
-
   const form = useForm<z.infer<typeof signUpSchema>>({
     resolver: zodResolver(signUpSchema),
     defaultValues: {
+      username:"",
       fullName: "",
       email: "",
       password: "",
-      avatar: avatar,
+      avatar: null,
+      coverImage:  null ,
+
     },
   });
 
-  function onSubmit(values: z.infer<typeof signUpSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
-  }
+  async function onSubmit(values: z.infer<typeof signUpSchema>) {
+    const formData = new FormData();
 
+    // Append text fields
+    formData.append("username", values.username);
+    formData.append("fullName", values.fullName);
+    formData.append("email", values.email);
+    formData.append("password", values.password);
+
+    // Append file fields
+    if (values.avatar) formData.append("avatar", values.avatar);
+    if (values.coverImage) formData.append("coverImage", values.coverImage);
+
+    try {
+      const response = await api.post("/users/register", formData, {
+        headers: { "Content-Type": "multipart/form-data" },//coz document is included
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error registering user:", error);
+    }
+  }
   return (
-    <div className="min-h-full w-full flex items-center justify-center bg-orange-50">
+  <div className="h-full w-full flex items-center justify-center bg-orange-50 overflow-y-auto">
+      <div className="h-full w-full flex items-center justify-center bg-orange-50 ">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
           Sign Up
         </h2>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
-            {/* fullname */}
+            {/* fullname  and username*/}
+            <div className=" flex justify-between">
             <FormField
               control={form.control}
-              name="fullName"
+              name="username"
               render={({ field }) => (
                 <FormItem>
                   <FormLabel className="block text-sm font-medium text-gray-700">
@@ -66,6 +84,27 @@ const SignupPage = () => {
                 </FormItem>
               )}
             />
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="block text-sm font-medium text-gray-700">
+                    Full Name
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+                      placeholder="Enter your full name"
+                      {...field}
+                      required
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            </div>
+            
             {/* email */}
             <FormField
               control={form.control}
@@ -110,42 +149,69 @@ const SignupPage = () => {
             <FormField
               control={form.control}
               name="avatar"
-              render={({ field }) => (
+              render={({ field: { value, onChange, ...fieldProps } }) => (
                 <FormItem>
-                  <FormLabel className="block text-sm font-medium text-gray-700">
-                    Avatar
-                  </FormLabel>
+                  <FormLabel>Avatar</FormLabel>
                   <FormControl>
                     <Input
-                    
+                      {...fieldProps}
+                      type="file"
                       className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
                       placeholder="Enter your avater"
-                      {...field}
                       required
+                      accept="image/*, application/pdf"
+                      onChange={(event) =>
+                        onChange(event.target.files && event.target.files[0] || null)
+                      }
                     />
                   </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* cover image  */}
+            <FormField
+              control={form.control}
+              name="coverImage"
+              render={({ field: { value, onChange, ...fieldProps } }) => (
+                <FormItem>
+                  <FormLabel>Cover Image</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...fieldProps}
+                      type="file"
+                      className="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
+                      placeholder="Enter your avater"
+                      accept="image/*, application/pdf"
+                      onChange={(event) =>
+                        onChange(event.target.files && event.target.files[0] || null)
+                      }
+                    />
+                  </FormControl>
+                  <FormMessage />
                 </FormItem>
               )}
             />
             {/* Signup Button */}
-<button
-  type="submit"
-  className="w-full bg-blue-600 text-white py-2 rounded-md font-semibold hover:bg-blue-700 transition duration-200"
->
-  Sign Up
-</button>
+            <Button
+              type="submit"
+              className="w-full bg-blue-600 text-white py-2 rounded-md font-semibold hover:bg-blue-700 transition duration-200"
+            >
+              Sign Up
+            </Button>
 
-{/* Login Link */}
-<div className="text-sm text-gray-600 mt-4 text-center">
-  Already have an account?{" "}
-  <Link href="/login" className="text-blue-600 hover:underline">
-    Log in
-  </Link>
-</div>
+            {/* Login Link */}
+            <div className="text-sm text-gray-600 mt-4 text-center">
+              Already have an account?{" "}
+              <Link href="/login" className="text-blue-600 hover:underline">
+                Log in
+              </Link>
+            </div>
           </form>
         </Form>
       </div>
     </div>
+  </div>
   );
 };
 
--- client/src/app/schemas/signupSchema.ts
+++ client/src/app/schemas/signupSchema.ts
@@ -1,19 +1,12 @@
 import { z } from "zod";
 
+const image = z.instanceof(File).nullable()
+
 export const signUpSchema = z.object({
+  username: z.string().min(3).max(50),
   fullName: z.string().min(3).max(50),
   email: z.string().email(),
   password: z.string().min(8).max(50),
-  avatar: z
-  .instanceof(File)
-  .refine((file) => file?.size <= 5 * 1024 * 1024, { // max size 5MB
-    message: "File size must be less than 5MB",
-  })
-  .refine(
-    (file) => ["image/jpeg", "image/png"].includes(file?.type), // allowed file types
-    {
-      message: "Only .jpg or .png files are allowed",
-    }
-  )
-  .optional(), 
+  avatar: image,
+  coverImage: image
 });
